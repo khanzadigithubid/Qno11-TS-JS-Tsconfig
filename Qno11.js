821a5dca6031136a3c31984662acbe4ac5f08c8a
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:34:14 +0500

    Qno11 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno11 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno11.js
@@ -0,0 +1,8 @@
+"use strict";
+/* Names: Store the names of a few of your friends in an array called names. Print each
+person's name by accessing each element in the list, one at a time.*/
+let names = ["Khanzadi", "Sapna", "Ayesha", "Kiran"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
---
+++ Qno11.ts
@@ -0,0 +1,9 @@
+/* Names: Store the names of a few of your friends in an array called names. Print each 
+person's name by accessing each element in the list, one at a time.*/
+
+let names: string[] = ["Khanzadi", "Sapna", "Ayesha", "Kiran"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+ 
